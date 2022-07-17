2a19b800f56e981469734361ec29eb857178a8b2
SW Tester <swtest150@gmail.com>
2022-07-17 13:14:52 +0200

    4th commit

---
+++ Excercise/exc2.py
@@ -0,0 +1,15 @@
+## 9/5 * celsius + 32
+## T(°C) = (T(°F) - 32) × 5/9
+
+c = eval(input("Enter the temperature in Celsius: "))
+f = 9/5* c + 32
+
+print("In Fahrenheit: ", f)
+
+f = eval(input("Enter the temperature in Fahrenheit: "))
+c = (f - 32) * 5/9
+
+print("In Celsius: ", c)
+
+
+
