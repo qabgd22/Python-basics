e1dc0990bf9df8094db85e927dd5a67fa6acdb07
SW Tester <swtest150@gmail.com>
2022-08-04 22:48:46 +0200

    7th commit

---
+++ Excercise/exc5.py
@@ -0,0 +1,31 @@
+from random import randint
+
+for i in range(5):
+    try:
+        broj1 = randint(1,10)
+        broj2 = randint(1,10)
+        print(broj1, '*', broj2, '=', end=' ')
+        rez = eval(input())
+
+        if rez == (broj1 * broj2):
+            print('Tačno')
+        else:
+            print('Netačno! Tačan odgovor je:', broj1 * broj2)
+
+    except SyntaxError:
+        print('SyntaxError - Nije unet broj')
+    except NameError:
+        print('NameError - Unet tekst') 
+print()
+
+# Fibonacci
+Fpr=0
+Fsl=1
+
+n = eval(input('Unesi broj članova niza: '))
+        
+for i in range(n):
+    print(Fpr, end=' ')
+    f = Fpr + Fsl
+    Fpr = Fsl
+    Fsl = f
